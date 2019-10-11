05aa96a5bb7b93207200abff90738d3c91550c93
bmsteven <bmsteve96@gmail.com>
2019-10-12 01:52:26 +0300

    updated some stuffs

--- codemunit/src/landing-page/components/News.js
+++ codemunit/src/landing-page/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import aos from "aos/dist/aos.css"
 
 import CarrierNews from "./CarrierNews";
 import TechNews from "./TechNews";
@@ -10,12 +11,12 @@ export default class News extends Component {
       <div className="blog-news">
         <div className="after"></div>
           <div className="container">
-            <h1>News and resources</h1>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium quibusdam molestiae temporibus, commodi repellat consectetur? Magni veniam laborum, eaque non commodi nam repellat laboriosam suscipit quibusdam accusamus adipisci voluptatibus velit!</p>
-            <Link to="/blog">View all stories</Link>
+            <h1 data-aos="fade-in">News and resources</h1>
+            <p data-aos="fade-in">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium quibusdam molestiae temporibus, commodi repellat consectetur? Magni veniam laborum, eaque non commodi nam repellat laboriosam suscipit quibusdam accusamus adipisci voluptatibus velit!</p>
+            <Link to="/blog" data-aos="fade-in">View all stories</Link>
             <input type="radio" name="n" id="n1" defaultChecked />
             <input type="radio" name="n" id="n2" />
-            <div className="news-header">
+            <div className="news-header" data-aos="fade-in">
               <label htmlFor="n1" className="carrier-news">
                 Carrier
               </label>
