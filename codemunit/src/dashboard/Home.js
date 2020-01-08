369fe97b7fa0fb232bfaf8d66e0f9c4baff087dd
benedict <bmsteve96@gmail.com>
2020-01-09 01:21:33 +0300

    added a logot btn

--- codemunit/src/dashboard/Home.js
+++ codemunit/src/dashboard/Home.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react'
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logout } from "../redux/actions/auth";
 import "../styles/dashboard/style.css"
 
 //pages
 import Sidebar from "./components/Sidebar"
 import Main from "./components/Main"
 
-export default class Dashboard extends Component {
-    componentDidMount() {
-        document.title = "Dashboard - Codemunit";
-    }
-    render() {
+const Dashboard = ({ auth: { isAuthenticated, loading }, logout }) => {
         return (
             <div className="dashboard dashboard-home">
                 <Sidebar/>
                 <Main/>
                 <div className="aside-right">
                     this is another container to be aligned on the right hand side for additional features
+                    <button onClick={logout} className="btn-primary">logout</button>
                 </div>
             </div>
         )
-    }
 }
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+});
+
+Dashboard.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+export default connect(mapStateToProps, { logout })(Dashboard);
